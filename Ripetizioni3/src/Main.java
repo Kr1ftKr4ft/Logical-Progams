55e2daa9cddbd6190387fa387c90869930c04fb6
Johnny <19giovanni.dandrea98@gmail.com>
2019-05-23 10:35:56 +0200

    Optimized some code

--- Ripetizioni3/src/Main.java
+++ Ripetizioni3/src/Main.java
@@ -1,11 +1,16 @@
+/*
+ * Author: john4s
+ * Version: V1.1
+ * Logical exercise for dummies(Me too)
+ */
 
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		View view = new View();
-		int scelta = view.menu();
-		switch (scelta) {
+		int scelta = view.menu();	//instead of using this useless variable you could've just written:
+		switch (scelta) {			//switch(view.menu()) {
 		case 1:
 			String num = view.binaryControl("Insert a binary number: ");
 			view.binaryConversion(num);
@@ -14,8 +19,16 @@ public class Main {
 			int num2 = view.readInt("Insert a decimal number: ");
 			view.decimalConversion(num2);
 			break;
+		case 3:
+			int numa = view.readInt("Insert first number: ");
+			int numb = view.readInt("Insert second number: ");
+			view.sum(numa, numb);
+			break;
+		case 4:
+			view.randomArrayNum();
+			break;
 		}
-		
+
 	}
 
 }
--- Ripetizioni3/src/View.java
+++ Ripetizioni3/src/View.java
@@ -1,3 +1,4 @@
+import java.util.Random;
 import java.util.Scanner;
 
 public class View {
@@ -5,14 +6,17 @@ public class View {
 	public final String ERROR = "Error in the App!";
 	private Scanner input;
 
+	// Options menu
 	public int menu() {
-		System.out.println("Program by Giovanni D'Andrea  -kr1ftkr4ft  V1.0");
+		System.out.println("Program by Giovanni D'Andrea  -john4s  V1.1 23/05/2019");		//...
 		System.out.println("1)Binary to decimal");
 		System.out.println("2)Decimal to binary");
-		System.out.println("3)Decimale a binario");
+		System.out.println("3)Sum");
+		System.out.println("4)Random number array");
 		return readInt("What do you want to do?");
 	}
 
+	// Control the type Integer
 	public int readInt(String s) {
 		input = new Scanner(System.in);
 		int num = 0;
@@ -30,7 +34,7 @@ public class View {
 		return num;
 	}
 
-	
+	// Control a string of number if there are a 0 and 1
 	public String binaryControl(String s) {
 		System.out.println(s);
 		String number;
@@ -49,7 +53,7 @@ public class View {
 		return number;
 	}
 
-	
+	// Convert a decimal number to binary number
 	public void binaryConversion(String number) {
 		int j = 0;
 		System.out.println("Bin: " + number);
@@ -61,20 +65,35 @@ public class View {
 		}
 		System.out.println("Num: " + result);
 	}
-	
-	
+
+	// Convert a binary number to decimal number
 	public void decimalConversion(int number) {
-		boolean flag;
-		do {
-			flag = false;
-			try {
 				String num = Integer.toBinaryString(number);
-				System.out.println("Bin: " + "["+num+"]");
-			} catch (NumberFormatException e) {
-				System.out.println(ERROR);
-				flag = true;
-			}
-		} while (flag);
+				System.out.println("Bin: " + "[" + num + "]");
 	}
-	
+
+	// sum between two numbers passed from
+	public void sum(int num1, int num2) {
+		System.out.println("First number: " + num1 + ", Second number: " + num2);
+		System.out.println("The sum is: " + (num1+num2)); //you could've just put (num1+num2) btw
+	}
+
+	// insert a random number(0 - 100) in an array of 10 positions
+	public void randomArrayNum() {		//why does the function return an int? Johnny: "It doesn't."
+		Random random = new Random();
+		int i;
+		int pos = 0;
+		int num[] = new int[10];
+		int max = num[0];
+		for (i = 0; i < num.length; i++) {
+			num[i] = random.nextInt(100);
+			if (num[i] > max) {
+				max = num[i];
+				pos = i;
+			}
+			System.out.print(num[i] + ",");
+		}
+		System.out.println("\nMax number: " + max + ", in position: " + pos);
+	}
+
 }
